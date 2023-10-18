98829067f5db9400f025d5e920632efdfb72adcb
TyreszB <127250161+TyreszB@users.noreply.github.com>
2023-10-17 21:27:57 -0400

    learned callbacks

--- 1-node-farm/starter/index.js
+++ 1-node-farm/starter/index.js
@@ -1,7 +1,25 @@
 const fs = require("fs");
+// Blocking
+// const textIn = fs.readFileSync("./txt/input.txt", "utf-8");
+// console.log(textIn);
 
-const textIn = fs.readFileSync("./txt/input.txt", "utf-8");
-console.log(textIn);
+// const textOut = `This is what we know about the avacado: ${textIn} \nCreated on ${Date.now()}`;
+// fs.writeFileSync("./txt/output.txt", textOut);
 
-const textOut = `This is what we know about the avacado: ${textIn} \nCreated on ${Date.now()}`;
-fs.writeFileSync("./txt/output.txt", textOut);
+// Async
+
+fs.readFile("./txt/start.txt", "utf-8", (err, data1) => {
+  fs.readFile(`./txt/${data1}.txt`, "utf-8", (err, data2) => {
+    console.log(data2);
+    fs.readFile(`./txt/append.txt`, "utf-8", (err, data3) => {
+      console.log(data3);
+
+      fs.writeFile(
+        "./txt/final.txt",
+        `${data2}\n${data3}`,
+        "utf-8",
+        (err) => {}
+      );
+    });
+  });
+});

b650d4c9814b31e2c7f71fb1575043f6f1a3f8a9
TyreszB <127250161+TyreszB@users.noreply.github.com>
2023-11-08 22:07:07 -0500

    added query function to find tours

--- 4-natours/starter/controllers/tourContoller.js
+++ 4-natours/starter/controllers/tourContoller.js
@@ -1,8 +1,15 @@
+/* eslint-disable node/no-unsupported-features/es-syntax */
 const Tour = require('../models/tourModel');
 
 exports.getAllTours = async (req, res) => {
   try {
-    const tours = await Tour.find();
+    const queryObj = { ...req.query };
+    const excludedFields = ['page', 'sort', 'limit', 'fields'];
+    excludedFields.forEach((el) => delete queryObj[el]);
+
+    const query = Tour.find(queryObj);
+
+    const tours = await query;
 
     res.status(200).json({
       status: 'success',

5281a99a03edc0ec8b340a060a2365982984ea53
TyreszB <127250161+TyreszB@users.noreply.github.com>
2023-11-14 23:27:34 -0500

    implemented a aggregation for finding tours each month from data

--- 4-natours/starter/routes/tourRoutes.js
+++ 4-natours/starter/routes/tourRoutes.js
@@ -8,11 +8,13 @@ const {
   deleteTour,
   aliasTopTours,
   getTourStats,
+  getMonthlyPlan,
 } = require('../controllers/tourContoller');
 
 const router = express.Router();
 
 router.route('/top-5-cheap').get(aliasTopTours, getAllTours);
+router.route('/monthly-plan/:year').get(getMonthlyPlan);
 router.route('/tour-stats').get(getTourStats);
 router.route('/').get(getAllTours).post(createTour);
 router.route('/:id').get(getTour).patch(updateTour).delete(deleteTour);

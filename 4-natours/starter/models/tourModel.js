d6506f51dcf22d2649f7b93f610e2be0089cb6be
TyreszB <127250161+TyreszB@users.noreply.github.com>
2023-11-22 16:50:45 -0500

    using middleware

--- 4-natours/starter/models/tourModel.js
+++ 4-natours/starter/models/tourModel.js
@@ -63,9 +63,12 @@ const tourSchema = new mongoose.Schema(
 
 tourSchema.pre(/^find/, function (next) {
   this.find({ secretTour: { $ne: true } });
+
+  this.start = Date.now();
   next();
 });
 tourSchema.post(/^find/, (docs, next) => {
+  console.log(`Query took ${Date.now() - this.start} miliseconds!`);
   console.log(docs);
   next();
 });
